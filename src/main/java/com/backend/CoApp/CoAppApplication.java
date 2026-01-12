10b019df48cb3e98d5c980b0f0eddbd08b7bd94f
Ngo Bao <ngotrangiabao2003@gmail.com>
2026-01-12 07:18:02 -0600

    Init: Set up spring boot env

---
+++ src/main/java/com/backend/CoApp/CoAppApplication.java
@@ -0,0 +1,13 @@
+package com.backend.CoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/CoApp/CoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.CoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
