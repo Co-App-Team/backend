e04bcf9d9b20749f84d5790f67eebc90aa4d319e
gloox <geetloomba79@gmail.com>
2026-01-19 12:44:16 -0600

    added mocks to unit tests to fix test failure

--- src/test/java/com/backend/coapp/CoAppApplicationTests.java
+++ src/test/java/com/backend/coapp/CoAppApplicationTests.java
@@ -2,12 +2,24 @@ package com.backend.coapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import com.backend.coapp.repository.UserRepository;
 
 @SpringBootTest
 class CoAppApplicationTests {
 
+	@MockitoBean
+	private MongoTemplate mongoTemplate;
+
+	@MockitoBean
+	private GridFsTemplate gridFsTemplate;
+
+	@MockitoBean
+	private UserRepository userRepository;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
