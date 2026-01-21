81b437939ab4c43edde20b8a2a9e7648d37d3fcc
Ngo Bao <ngotrangiabao2003@gmail.com>
2026-01-20 22:55:57 -0600

    Refactored UserModel and extended it

---
+++ src/main/java/com/backend/coapp/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.backend.coapp.dto;
+
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+    public UserDTO(String firstName, String lastName, String email, int id){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
