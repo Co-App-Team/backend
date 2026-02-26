d219012da2bce94460437229ef439e7b50473656
Ngo Bao <ngotrangiabao2003@gmail.com>
2026-02-26 12:36:25 -0600

    Added exception handler

--- src/main/java/com/backend/coapp/config/SecurityConfig.java
+++ src/main/java/com/backend/coapp/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.backend.coapp.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -35,6 +37,10 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.cors(Customizer.withDefaults())
         .csrf(csrf -> csrf.disable())
+        .exceptionHandling(
+            exception ->
+                exception.authenticationEntryPoint(customAuthenticationEntryPoint()) // ← add this
+            )
         .authorizeHttpRequests(
             auth ->
                 auth.requestMatchers(HttpMethod.OPTIONS, "/**")
@@ -50,5 +56,16 @@ public class SecurityConfig {
         .authenticationProvider(authenticationProvider)
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
+  }
+
+  @Bean
+  public AuthenticationEntryPoint customAuthenticationEntryPoint() {
+    return (request, response, authException) -> {
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+      response.setContentType("application/json");
+      response
+          .getWriter()
+          .write("{\"error\": \"UNAUTHORIZED\", \"message\": \"Authentication is required.\"}");
+    };
   }
 }
