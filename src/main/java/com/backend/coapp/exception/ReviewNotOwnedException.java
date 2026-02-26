f0de3f6ffd90b218dba42a25dc26c659a9245544
Ngo Bao <ngotrangiabao2003@gmail.com>
2026-02-25 19:49:45 -0600

    Removed ReviewNotOwnedException, make concurrency test deterministic, add GenAI credit for help writing unit tests

--- src/main/java/com/backend/coapp/exception/ReviewNotOwnedException.java
+++
@@ -1,9 +0,0 @@
-package com.backend.coapp.exception;
-
-/** thrown when a user tries to update/delete a review they don't own */
-public class ReviewNotOwnedException extends RuntimeException {
-
-  public ReviewNotOwnedException(String action) {
-    super("You can only " + action + " your own reviews.");
-  }
-}
