70b4b87655d0a00c89330acd955afd737d08e5f1
gloox <geetloomba79@gmail.com>
2026-03-12 11:28:46 -0500

    addressed bao comment: fixed exception

--- src/main/java/com/backend/coapp/exception/global/UserNotFoundException.java
+++ src/main/java/com/backend/coapp/exception/global/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.backend.coapp.exception.global;
 
 public class UserNotFoundException extends RuntimeException {
   public UserNotFoundException() {
-    super(String.format("Could not find user"));
+    super("Could not find user");
   }
 }
