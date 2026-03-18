834bd9a9f01d4109e27f15c68cde53ddb51867de
Ngo Bao <ngotrangiabao2003@gmail.com>
2026-03-18 13:29:35 -0500

    Added Exception handler for invalid JSON format for date

--- src/main/java/com/backend/coapp/model/enumeration/RequestErrorCode.java
+++ src/main/java/com/backend/coapp/model/enumeration/RequestErrorCode.java
@@ -2,5 +2,6 @@ package com.backend.coapp.model.enumeration;
 
 /** All possible error codes returned for bad REST API requests. */
 public enum RequestErrorCode {
-  REQUEST_HAS_NULL_OR_EMPTY_FIELD
+  REQUEST_HAS_NULL_OR_EMPTY_FIELD,
+  INVALID_FORMAT_FIELD
 }
